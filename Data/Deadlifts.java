56bed728c41a8425e35fcd0b4b906bc16a750509
Jamie Patrick Ching <sazandora.muncher@gmail.com>
2019-11-04 14:50:33 -0700

    added documentation

--- Data/Deadlifts.java
+++ Data/Deadlifts.java
@@ -1,11 +1,24 @@
+/**The class that holds Deadlifts data.
+ */
 public class Deadlifts extends Lifts
 {
+	/**Constructor with null values in both reps
+	 * and weight.
+	 */
 	public Deadlifts() {
 		super("Deadlifts");
 	}
+	/**Constructor with a defined reps value.
+	 * @param	o	The value of the reps.
+	 */
 	public Deadlifts(int o) {
 		super(o, "Deadlifts");
 	}
+	/**Constructor with a defined reps value,
+	 * and a weight value.
+	 * @param	o	The value of the reps.
+	 * @param	p	The value of the weight.
+	 */
 	public Deadlifts(int o, double p) {
 		super(o, p, "Deadlifts");
 	}
