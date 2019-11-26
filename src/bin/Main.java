b654860273d78bedcfab2fcf6b144cd303566952
Jamie Patrick Ching <jamiepatrick.ching@gmail.com>
2019-11-25 19:28:37 -0700

    got the main file

---
+++ src/bin/Main.java
@@ -0,0 +1,13 @@
+/**The main class that runs the TUI.
+ */
+public class Main
+{
+	/**Main object that is run at runtime.
+	 * Maybe might take in user data if needed.
+	 */
+	public static void main(String[] args) {
+		UserData z = new UserData();
+		Home a = new Home();
+		a.Menu_Main(z);
+	}
+}
